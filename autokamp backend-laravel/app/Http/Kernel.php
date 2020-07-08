935ea5149b12f5c657bfcef6d0dd1a73b3495135
Aleks Ruzic <aleksruzic1993@gmail.com>
2020-07-08 12:50:54 +0200

    Unutar Kernela registrirana ruta za Middleware admin i campcontrol
    Te rute su povezane sa IsAdmin::class i IsCampControl::class unutar kojih će se provjeravat ako je logirani korisnik admin ili kontrolor

--- autokamp backend-laravel/app/Http/Kernel.php
+++ autokamp backend-laravel/app/Http/Kernel.php
@@ -14,13 +14,13 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        
     ];
 
     /**
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-    ];
+        'admin' => \App\Http\Middleware\IsAdmin::class, 
+        'campcontrol' => \App\Http\Middleware\IsCampControl::class,
+        ];
 }
+
