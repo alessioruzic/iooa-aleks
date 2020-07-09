d30cf52776302482bad852d524ecaf1a4652003f
Aleks Ruzic <aleksruzic1993@gmail.com>
2020-07-09 11:59:38 +0200

    Kreirani pogledi za pretraživanje gostiju po parcelama na dan

---
+++ autokamp backend-laravel/resources/views/cmskontrola/ispis_gost_parcela.blade.php
@@ -0,0 +1,48 @@
+@extends('cmskontrola.controlmaster')
+
+@section('content')
+<div class="d-flex justify-content-end mb-2">
+</div>
+  <div class="card card-defatult">
+    <div class="card-header">PREGLED GOSTIJU PO PARCELAMA</div>
+    @if($gostiparcela->count()>0)
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <th>PARCELA BROJ</th>
+                <th>GOST</th>
+                <th>DOLAZAK</th>
+                <th>ODLAZAK</th>
+                <th></th>
+            </thead>
+            <tbody>
+                @foreach($gostiparcela as $gostparcela)
+                    <tr>
+                        <td>
+                            {{ $gostparcela->parcela->brojParcela }}
+                        </td>
+                        <th>
+                            {{ $gostparcela->gost->name }} {{ $gostparcela->gost->surname }}
+                        </th>
+                        <th>
+                          {{ $gostparcela->datumDolazak }}
+                      </th>
+                      <th>
+                        {{ $gostparcela->datumOdlazak }}
+                      </th>
+                        <th>
+                        <td>
+                           
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+@else
+<div class="card-header" mt-5>
+    <h4 class="text-center">Nema podataka o Gostiju.</h4>
+    </div>
+@endif
+
+@endsection
